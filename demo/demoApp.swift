bd54352b0ba34fc9411def1c262759fa342f27d7
Smarann Khadka <smarann.khadka@avionte.com>
2024-12-15 19:37:56 +0545

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by PL Smarann Khadka on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demoTests/demoTests.swift
@@ -0,0 +1,16 @@
+//
+//  demoTests.swift
+//  demoTests
+//
+//  Created by PL Smarann Khadka on 15/12/2024.
+//
+
+import Testing
+
+struct demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
